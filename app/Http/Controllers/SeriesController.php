c9b1a9a60092f21c4d452d34fcbb0e9d22254340
Weslley Santo <weslleyesanto@gmail.com>
2022-05-23 18:25:58 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index() {
+        $series = Serie::all();
+
+        return view('series.index', compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nome = $request->nome;
+        $serie = Serie::create([
+            'nome' => $nome,
+        ]);
+        echo "Série com id {$serie->id} criada: {$serie->nome}";
+    }
+}
